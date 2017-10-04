1ddd2184bac02d73c11ea20da3d6858219144acf
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2017-10-04 10:57:33 -0400

    a few updates

--- inst/unitTests/test_matrix.R
+++ inst/unitTests/test_matrix.R
@@ -1,5 +1,10 @@
 test_bands <- function() {
-    checkTrue(TRUE)
-    ## checkEquals
+    mat <- matrix(1:9, 3, 3)
+    checkEquals(band(mat, band.no = 2), c(4,8))
+    band(mat, band.no) <- c(9,10)
+    checkEquals(mat[1,2], 9)
+    checkEquals(mat[2,1], 9)
+    checkEquals(mat[2,3], 10)
+    checkEquals(mat[3,2], 10)
 }
 
