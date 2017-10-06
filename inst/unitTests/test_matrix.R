3e2455cde9035965e4365a9678d2cea93fa88c4d
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2017-10-06 10:54:56 -0400

    finalizing man pages

--- inst/unitTests/test_matrix.R
+++ inst/unitTests/test_matrix.R
@@ -1,7 +1,7 @@
 test_bands <- function() {
     mat <- matrix(1:9, 3, 3)
     checkEquals(band(mat, band.no = 2), c(4,8))
-    band(mat, band.no) <- c(9,10)
+    band(mat, band.no = 2) <- c(9,10)
     checkEquals(mat[1,2], 9)
     checkEquals(mat[2,1], 9)
     checkEquals(mat[2,3], 10)
